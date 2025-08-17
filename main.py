f0ff0b83e24e54f7e50c74487d323ebeee96d89a
oaksoe <ozaw@oswego.edu>
2025-08-17 15:25:07 -0400

    almost done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
+import sys
 from pathlib import Path
+import random
 
 from game import monster
 from game.monster import Visitor
@@ -21,6 +23,7 @@ def generateVisitor():
     Visitor(visitors[key]['name'],visitors[key]['type'],visitors[key]['ans'])
         for key in visitors
     ]
+    random.shuffle(visitArray)
     return visitArray
 
 def inspect():
@@ -31,7 +34,27 @@ def ask_person():
     # Write function for asking the person
     pass
 
-def open_Door():
+def open_Door(visitor,count):
+    if visitor.type == "monster":
+        print("Game Over, you let a monster in.")
+        sys.exit()
+    else:
+        print(f"{visitor.name} did the business and left")
+        count += 1
+    return count
+
+def close_Door(visitor,count):
+    print("You asked the person to leave")
+    if visitor.type == "monster":
+        print(f"{visitor.name} said 'I will get you next timeeeee'")
+    else:
+        print("Okay, I am going to tell your mom ")
+
+    count +=1
+    return count
+
+def inventory():
+    #write a loop for displaying all the items in the inventory
     pass
 
 def start_game():
@@ -39,10 +62,30 @@ def start_game():
     print(game_info["start_description"])
     running = True
     count = 0
-    visitors =generateVisitor()
+    visitors = generateVisitor()
+    inventory = []
+    removedItems = set()
     while running:
+        visitor = visitors[count]
         cm = input("Enter command: ")
-        count += 1
+        if cm == "open":
+            count = open_Door(visitor,count)
+        if cm == "close":
+            count = close_Door(visitor,count)
+
+        if count >= len(visitors):
+            print("No more visitors are coming")
+            while True:
+                choice = input("Do you want to play again? (yes/no)")
+                if choice == 'yes':
+                    count = 0
+                    visitors = generateVisitor()
+                    print("Make sure to survive")
+                    break
+                else:
+                    print("Thanks for playing")
+                    running = False
+                    break
 
         #input
 
